8e9f5700a43c965d04cfaa889d66fd8d36cc8bf6
gygy <67543454+Gayun00@users.noreply.github.com>
2024-04-03 20:38:55 +0900

    Add: 홈 페이지 > 컨텐츠 조회 무한 스크롤 (#18)
    * Add: useIntersectionObserver 커스텀 훅
    
    * Add: InfiniteScrollTrigger 컴포넌트
    
    * Add: 홈 페이지 무한 스크롤 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import { useContentsInfiniteQuery } from "@/queries/contents";
 import Header from "@/components/layouts/Header";
 import ContentCard from "@/components/cards/ContentCard";
 import MainLayout from "@/components/layouts/MainLayout";
+import InfiniteScrollTrigger from "@/components/InfiniteScrollTrigger";
 import "./globals.css";
 
 export default function Home() {
-  const { data } = useContentsInfiniteQuery();
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useContentsInfiniteQuery();
 
   return (
     <div className="min-w-[770px]">
       {/* TODO: layout 리팩토링 */}
+      {/* Suspense 및 Skeleton UI 추가 */}
       <Header />
       <MainLayout>
         {data?.pages
@@ -23,6 +26,12 @@ export default function Home() {
               {expand?.news && <ContentCard.RefContent {...expand.news} />}
             </ContentCard>
           ))}
+
+        <InfiniteScrollTrigger
+          fetchNextPage={fetchNextPage}
+          hasNextPage={hasNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+        />
       </MainLayout>
     </div>
   );
