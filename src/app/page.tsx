819d56fdb562f158969708a9a5dc54b06e6f5c2d
Gayun00 <gayunlee11@gmail.com>
2024-04-03 20:22:53 +0900

    Add: 홈페이지 api 응답값으로 콘텐츠 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 
-import { useSelector } from "react-redux";
+import { useContentsInfiniteQuery } from "@/queries/contents";
+import Header from "@/components/Header";
+import ContentCard from "@/components/cards/ContentCard";
+import MainLayout from "@/components/layouts/MainLayout";
 import "./globals.css";
 
 export default function Home() {
-  const user = useSelector((state) => state);
-  console.log(user);
-  return <div>homed page</div>;
+  const { data } = useContentsInfiniteQuery();
+
+  return (
+    <div className="min-w-[770px]">
+      {/* TODO: layout 리팩토링 */}
+      <Header />
+      <MainLayout>
+        {data?.pages
+          ?.flatMap(({ items }) => items)
+          .map(({ id, title, text, expand }) => (
+            <ContentCard key={id}>
+              <ContentCard.Author {...expand.author} />
+              <ContentCard.TextContent title={title} text={text} />
+              {expand?.news && <ContentCard.RefContent {...expand.news} />}
+            </ContentCard>
+          ))}
+      </MainLayout>
+    </div>
+  );
 }
