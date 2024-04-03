81f4aaf4bdee7ff51005f68c6fa36ad5027f0ad1
Gayun00 <gayunlee11@gmail.com>
2024-04-04 00:23:39 +0900

    Fix: Link 태그 위치 수정

--- src/components/cards/ContentCard/TextContent/index.tsx
+++ src/components/cards/ContentCard/TextContent/index.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { CardDescription, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 interface Props {
   title: string;
   text: string;
+  type: string;
+  id: string;
 }
 
-const TextContent = ({ title, text }: Props) => {
+const TextContent = ({ title, text, type, id }: Props) => {
   return (
     <div className="px-0 py-[15px] space-y-[15px]">
-      <CardTitle className="text-[24px] font-semibold text-ellipsis overflow-hidden whitespace-nowrap">
-        {title}
-      </CardTitle>
-      <CardDescription className="text-us-gray-text text-base text-ellipsis overflow-hidden whitespace-normal line-clamp-2">
-        {text}
-      </CardDescription>
+      <Link href={`/contents/${type}/${id}`}>
+        <CardTitle className="text-[24px] font-semibold text-ellipsis overflow-hidden whitespace-nowrap">
+          {title}
+        </CardTitle>
+        <CardDescription className="text-us-gray-text text-base text-ellipsis overflow-hidden whitespace-normal line-clamp-2">
+          {text}
+        </CardDescription>
+      </Link>
     </div>
   );
 };
--- src/components/cards/ContentCard/index.tsx
+++ src/components/cards/ContentCard/index.tsx
@@ -3,20 +3,13 @@ import { Card } from "@/components/ui/card";
 import RefContentCard from "./RefContentCard";
 import Author from "./Author";
 import TextContent from "./TextContent";
-import Link from "next/link";
 
 interface Props {
   children: ReactNode;
-  id: string;
-  type: string;
 }
 
-const ContentCard = ({ id, type, children }: Props) => {
-  return (
-    <Link href={`/contents/${type}/${id}`}>
-      <Card className="px-[35px] py-[30px] max-w-[570px]">{children}</Card>;
-    </Link>
-  );
+const ContentCard = ({ children }: Props) => {
+  return <Card className="px-[35px] py-[30px] max-w-[570px]">{children}</Card>;
 };
 
 ContentCard.RefContent = RefContentCard;
